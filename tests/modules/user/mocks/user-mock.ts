e7ddf19b25262f2b454bbdaa32ef96bdf97743e9
lucas_araujo <andrelucassaaraujo@gmail.com>
2021-07-25 20:44:10 -0400

    refactor: ensure UserMock with correct values

--- tests/modules/user/mocks/user-mock.ts
+++ tests/modules/user/mocks/user-mock.ts
@@ -1,9 +1,10 @@
-import faker from "faker"
+import faker from "faker";
+import { UserModel } from "../domain/models";
 
-export const UserMock = () => ({
+export const UserMock = (): Omit<UserModel, "id"> => ({
   name: faker.name.findName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
   status: "active",
-  role: "user"
-})
+  role: "user",
+});
