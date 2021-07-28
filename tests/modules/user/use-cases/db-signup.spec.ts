bddee242fec6a6772eeca8f905fcb43962568c1c
lucas_araujo <andrelucassaaraujo@gmail.com>
2021-07-27 21:59:22 -0400

    test: ensure email with correct format

--- tests/modules/user/use-cases/db-signup.spec.ts
+++ tests/modules/user/use-cases/db-signup.spec.ts
@@ -1,12 +1,15 @@
 import { DbSignup } from "@/modules/user/use-cases/db-signup";
+import { EmailValidatorFake } from "@/shared/adapters/email-validator-adapter/fake/email-validator-fake";
 import { UserMock } from "../mocks/user-mock";
 
 const params = UserMock();
 
 const makeSut = () => {
-  const sut = new DbSignup();
+  const emailValidatorFake = new EmailValidatorFake();
+  const sut = new DbSignup(emailValidatorFake);
   return {
     sut,
+    emailValidatorFake,
   };
 };
 
@@ -18,5 +21,20 @@ describe("==> signup", () => {
 
     expect(user.name).toEqual(params.name);
     expect(user.email).toEqual(params.email);
+  });
+
+  it("should throw a error if email format is invalid", async () => {
+    const { sut, emailValidatorFake } = makeSut();
+
+    jest
+      .spyOn(emailValidatorFake, "isValid")
+      .mockImplementationOnce(() => new Promise((reject) => reject(false)));
+
+    expect(
+      sut.add({
+        ...params,
+        email: "invalid_email",
+      })
+    ).rejects.toEqual(new Error("invalid email format"));
   });
 });
