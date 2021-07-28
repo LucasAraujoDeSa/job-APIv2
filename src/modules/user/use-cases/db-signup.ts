d9002393ad298b20de797a1b51e863b1afea34c1
lucas_araujo <andrelucassaaraujo@gmail.com>
2021-07-27 21:46:18 -0400

    feat: ensure email with correct format

--- src/modules/user/use-cases/db-signup.ts
+++ src/modules/user/use-cases/db-signup.ts
@@ -1,7 +1,16 @@
+import { EmailValidatorAdapter } from "@/shared/adapters/email-validator-adapter/email-validator-adapter";
 import { Signup } from "../domain/use-cases/signup";
 
 export class DbSignup implements Signup {
+  constructor(private readonly _emailValidator: EmailValidatorAdapter) {}
+
   public async add(params: Signup.Params): Promise<Signup.Result> {
+    const isValidEmail = await this._emailValidator.isValid(params.email);
+
+    if (!isValidEmail) {
+      throw new Error("invalid email format");
+    }
+
     return {
       id: "any",
       ...params,
