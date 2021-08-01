44aa179ccd18d812c95df84b08f3378eaa863453
lucas_araujo <andrelucassaaraujo@gmail.com>
2021-08-01 15:53:37 -0400

    feat: create newAccount if success

--- src/modules/user/use-cases/db-signup.ts
+++ src/modules/user/use-cases/db-signup.ts
@@ -1,12 +1,13 @@
 import { EmailValidatorAdapter, HashAdapter } from "@/shared/adapters";
 import { Signup } from "../domain/use-cases/signup";
-import { CheckByEmailRepository } from "../repositories";
+import { CheckByEmailRepository, AddAccountRepository } from "../repositories";
 
 export class DbSignup implements Signup {
   constructor(
     private readonly _emailValidatorAdapter: EmailValidatorAdapter,
     private readonly _checkByEmailRepository: CheckByEmailRepository,
-    private readonly _hashAdapter: HashAdapter
+    private readonly _hashAdapter: HashAdapter,
+    private readonly _addAccountRepository: AddAccountRepository
   ) {}
 
   public async add(params: Signup.Params): Promise<Signup.Result> {
@@ -28,10 +29,11 @@ export class DbSignup implements Signup {
 
     const hashedPassword = await this._hashAdapter.hash(params.password);
 
-    return {
-      id: "any",
+    const newAccount = await this._addAccountRepository.add({
       ...params,
       password: hashedPassword,
-    };
+    });
+
+    return newAccount;
   }
 }
