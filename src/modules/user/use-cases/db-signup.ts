e784b76136c907f5d1e7b68e89d57a523ca3d03f
lucas_araujo <andrelucassaaraujo@gmail.com>
2021-07-31 20:18:54 -0400

    feat: ensure user creation without email that already in use

--- src/modules/user/use-cases/db-signup.ts
+++ src/modules/user/use-cases/db-signup.ts
@@ -1,14 +1,26 @@
 import { EmailValidatorAdapter } from "@/shared/adapters/email-validator-adapter/email-validator-adapter";
 import { Signup } from "../domain/use-cases/signup";
+import { CheckByEmail } from "../repositories";
 
 export class DbSignup implements Signup {
-  constructor(private readonly _emailValidator: EmailValidatorAdapter) {}
+  constructor(
+    private readonly _emailValidator: EmailValidatorAdapter,
+    private readonly _checkByEmail: CheckByEmail
+  ) {}
 
   public async add(params: Signup.Params): Promise<Signup.Result> {
     const isValidEmail = await this._emailValidator.isValid(params.email);
 
     if (!isValidEmail) {
       throw new Error("invalid email format");
+    }
+
+    const alreadyEmailInUse = await this._checkByEmail.ifAlreadyInUse(
+      params.email
+    );
+
+    if (alreadyEmailInUse) {
+      throw new Error("email already in use");
     }
 
     return {
