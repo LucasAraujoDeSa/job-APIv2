f1c43f5b99c28bd2d10315a6b252b29b8c5ecbd7
lucas_araujo <andrelucassaaraujo@gmail.com>
2021-08-01 14:49:16 -0400

    feat: ensure secure passwords with hash

--- src/modules/user/use-cases/db-signup.ts
+++ src/modules/user/use-cases/db-signup.ts
@@ -1,11 +1,12 @@
-import { EmailValidatorAdapter } from "@/shared/adapters/email-validator-adapter/email-validator-adapter";
+import { EmailValidatorAdapter, HashAdapter } from "@/shared/adapters";
 import { Signup } from "../domain/use-cases/signup";
 import { CheckByEmail } from "../repositories";
 
 export class DbSignup implements Signup {
   constructor(
     private readonly _emailValidator: EmailValidatorAdapter,
-    private readonly _checkByEmail: CheckByEmail
+    private readonly _checkByEmail: CheckByEmail,
+    private readonly _hashAdapter: HashAdapter
   ) {}
 
   public async add(params: Signup.Params): Promise<Signup.Result> {
@@ -23,9 +24,12 @@ export class DbSignup implements Signup {
       throw new Error("email already in use");
     }
 
+    const hashedPassword = await this._hashAdapter.hash(params.password);
+
     return {
       id: "any",
       ...params,
+      password: hashedPassword,
     };
   }
 }
