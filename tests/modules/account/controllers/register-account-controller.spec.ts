0828f7e25f25716eb697179ab06405f92333eeb5
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2021-08-25 21:02:12 -0400

    test: make return 201 on success

--- tests/modules/account/controllers/register-account-controller.spec.ts
+++ tests/modules/account/controllers/register-account-controller.spec.ts
@@ -1,10 +1,29 @@
-import { EmailValidatorAdapterFake } from "@/../tests/shared/adapters";
+import {
+  EmailValidatorAdapterFake,
+  HashAdapterFake,
+  SmtpAdapterFake,
+} from "@/../tests/shared/adapters";
 import { RegisterAccountController } from "@/modules/account/application/controllers/register-account/register-account-controller";
+import { RegisterAccount } from "@/modules/account/domain/use-cases/register-account/register-account";
+import { AddAccountRepositoryFake, CheckByEmailRepositoryFake } from "../fakes";
 import { AccountMock } from "../mocks/account-mock";
 
 const makeSut = () => {
   const emailValidatorAdapterFake = new EmailValidatorAdapterFake();
-  const sut = new RegisterAccountController(emailValidatorAdapterFake);
+  const checkByEmailRepositoryFake = new CheckByEmailRepositoryFake();
+  const hashAdapterFake = new HashAdapterFake();
+  const addAccountRepositoryFake = new AddAccountRepositoryFake();
+  const smtpAdapterFake = new SmtpAdapterFake();
+  const registerAccount = new RegisterAccount(
+    checkByEmailRepositoryFake,
+    hashAdapterFake,
+    addAccountRepositoryFake,
+    smtpAdapterFake
+  );
+  const sut = new RegisterAccountController(
+    emailValidatorAdapterFake,
+    registerAccount
+  );
   return {
     sut,
     emailValidatorAdapterFake,
@@ -24,5 +43,24 @@ describe("==> register account controller", () => {
 
     expect(httpResponse.status_code).toEqual(400);
     expect(httpResponse.body).toEqual("Invalid email format");
+  });
+
+  it("should 400 if fields is missing", async () => {
+    const { sut } = makeSut();
+
+    const httpResponse = await sut.handle({
+      ...params,
+      email: "",
+    });
+
+    expect(httpResponse.status_code).toEqual(400);
+  });
+
+  it("should 201 on success", async () => {
+    const { sut } = makeSut();
+
+    const httpResponse = await sut.handle(params);
+
+    expect(httpResponse.status_code).toEqual(201);
   });
 });
