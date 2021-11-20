68aa756751bfa0550b56ca382636291a8b3ea8c7
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2021-11-20 16:37:04 -0400

    chore: add query to uuid osp extensions

--- src/infra/database/postgres/migrations/1637428145571-account.ts
+++ src/infra/database/postgres/migrations/1637428145571-account.ts
@@ -2,6 +2,7 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class account1637428145571 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
     await queryRunner.createTable(
       new Table({
         name: "account",
