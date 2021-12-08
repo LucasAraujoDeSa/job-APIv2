d26210314bf56a48296a3f7b3bb6e9650f18f1b8
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2021-12-08 13:14:25 -0400

    feat: make update-access-token contract

--- src/modules/account/domain/contracts/repositories/index.ts
+++ src/modules/account/domain/contracts/repositories/index.ts
@@ -1,2 +1,4 @@
 export * from "./check-by-email-repository-contract";
 export * from "./add-account-repository-contract";
+export * from "./get-account-by-email-contract";
+export * from "./update-access-token-contract";
