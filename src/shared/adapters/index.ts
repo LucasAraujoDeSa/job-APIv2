c78f9d47ff1405efae4630b4e0851cc6e510f732
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2021-12-08 13:16:05 -0400

    feat: make encrypter adapter contract

--- src/shared/adapters/index.ts
+++ src/shared/adapters/index.ts
@@ -1,3 +1,4 @@
 export * from "./email-validator-adapter/email-validator-adapter";
 export * from "./hash-adapter/hash-adapter";
 export * from "./smtp-adapter/smtp-adapter";
+export * from "./encrypter-adapter/encrypter-adapter";
