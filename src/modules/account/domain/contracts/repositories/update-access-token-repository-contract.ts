5a1f25106687f45a4bb2b08c53581bd0f0c7f42e
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2021-12-11 15:51:18 -0400

    feat: make update-access-token repository

--- src/modules/account/domain/contracts/repositories/update-access-token-repository-contract.ts
+++ src/modules/account/domain/contracts/repositories/update-access-token-repository-contract.ts
@@ -3,3 +3,7 @@ import { AccountModel } from "../../models";
 export interface UpdateAcessTokenRepositoryContract {
   update(id: string, token: string): Promise<AccountModel>;
 }
+
+export namespace UpdateAcessTokenRepositoryContract {
+  export type Output = AccountModel;
+}
