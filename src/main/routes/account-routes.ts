7e1eba2c3180475344682fa8643080b0435e7e4c
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2021-12-11 16:07:04 -0400

    feat: make login route

--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { register_account_controller_factory } from "@/main/factories/controllers/account/register-account-controller-factory";
+import {
+  register_account_controller_factory,
+  login_controller_factory,
+} from "@/main/factories/controllers/account";
 import { adaptRoute } from "../adapters";
 
 export default (router: Router): void => {
   router.post("/singup", adaptRoute(register_account_controller_factory()));
+  router.post("/login", adaptRoute(login_controller_factory()));
 };
