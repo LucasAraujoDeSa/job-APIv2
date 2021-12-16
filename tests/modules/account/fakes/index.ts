d06923864461115a7770a91933bf1de0689f8632
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2021-12-16 17:05:23 -0400

    test: make get-account-by-id fake

--- tests/modules/account/fakes/index.ts
+++ tests/modules/account/fakes/index.ts
@@ -2,3 +2,4 @@ export * from "./check-by-email-repository-fake";
 export * from "./add-account-repository-fake";
 export * from "./get-account-by-email-repository-fake";
 export * from "./update-access-token-repository-fake";
+export * from "./get-account-by-id-repository-fake";
